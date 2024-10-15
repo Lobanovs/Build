c0528689c7892742bd219e4bd18e2b5dd8d3162f
Semyon Lobanov <161553757+Lobanovs@users.noreply.github.com>
2024-10-15 18:30:54 +0700

    Add files via upload

---
+++ Build/Builder/Builder/Models/Computer.cs
@@ -0,0 +1,28 @@
+﻿using Builder.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Models
+{
+    public class Computer
+    {
+        public CPU Cpu { get; set; }
+        public Motherboard Motherboard { get; set; }
+        public RAM Ram { get; set; }
+        public Storage Storage { get; set; }
+        public GPU Gpu { get; set; }
+
+        public override string ToString()
+        {
+            return $"Computer: \n" +
+                   $"CPU: {Cpu}\n" +
+                   $"Motherboard: {Motherboard}\n" +
+                   $"RAM: {Ram}\n" +
+                   $"Storage: {Storage}\n" +
+                   $"GPU: {Gpu}";
+        }
+    }
+}
---
+++ Build/Builder/Builder/Program.cs
@@ -0,0 +1,28 @@
+﻿using Builder.Builders;
+
+namespace Builder
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cpu = new CPU { Model = "Intel i5" };
+            var motherboard = new Motherboard { Model = "ASUS Prime" };
+            var ram = new RAM { Model = "Kingston 16GB" };
+            var storage = new Storage { Model = "Samsung SSD 512GB" };
+            var gpu = new GPU { Model = "NVIDIA GTX 1660" };
+
+            var computerBuilder = new ComputerBuilder();
+            var computer = computerBuilder
+                .SetCPU(cpu)
+                .SetMotherboard(motherboard)
+                .SetRAM(ram)
+                .SetStorage(storage)
+                .SetGPU(gpu)
+                .Build();
+
+            Console.WriteLine(computer);
+        }
+    }
+}
